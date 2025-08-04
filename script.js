5f575b7a7ccda340a287379009a01ff4ff39f3e7
sarthak-23-aps <sg3212568@gmail.com>
2025-08-04 07:47:30 -0700

    mfhejjuehejehehje

---
+++ script.js
@@ -0,0 +1,131 @@
+let allimages = document.querySelector(".imageslider");
+let image = document.querySelectorAll(".img");
+let index = 0;
+let prevbtn = document.querySelector("#prevbtn")
+let nxtbtn = document.querySelector("#nxtbtn")
+
+prevbtn.addEventListener("click", function () {
+    image.forEach(function (ima) {
+        ima.classList.remove("active")
+    })
+    index--;
+    if (index < 0) {
+        index = image.length - 1
+    }
+    image[index].classList.add("active")
+
+})
+
+nxtbtn.addEventListener("click", function () {
+
+    index++;
+    if (index >= image.length) {
+        index = 0
+    }
+    image.forEach(function (ima) {
+        ima.classList.remove("active")
+    })
+    image[index].classList.add("active")
+})
+
+setInterval(function () {
+    image[index].classList.remove("active")
+    index++;
+    if (index >= image.length) {
+        index = 0;
+    }
+    image[index].classList.add("active")
+}, 7000)
+
+// for phone 
+
+let imagebox2 = document.querySelector(".imageslider2")
+
+let alldot = document.querySelectorAll(".dot")
+
+let image2 = document.querySelectorAll(".pic");
+
+let idx = 0
+
+setInterval(function () {
+    image2[idx].classList.remove("act")
+    alldot[idx].classList.remove("act")
+    idx++;
+    if (idx >= image2.length) {
+        idx = 0;
+    }
+    image2[idx].classList.add("act")
+    alldot[idx].classList.add("act")
+}, 5000)
+
+let startX = 0
+let endX = 0
+imagebox2.addEventListener("touchstart", function (e) {
+    startX = e.changedTouches[0].screenX
+})
+
+imagebox2.addEventListener("touchend", function (e) {
+    endX = e.changedTouches[0].screenX
+
+    let dist = endX - startX
+
+    if (dist > 50) {
+        image2[idx].classList.remove("act")
+        alldot[idx].classList.remove("act");
+        idx--;
+        if (idx < 0) {
+            idx = image2.length - 1
+        }
+        image2[idx].classList.add("act")
+        alldot[idx].classList.add("act");
+    } else if (dist < -50) {
+        image2[idx].classList.remove("act")
+        alldot[idx].classList.remove("act");
+        idx++;
+        if (idx >= image2.length) {
+            idx = 0
+        }
+        image2[idx].classList.add("act")
+        alldot[idx].classList.add("act");
+    }
+})
+
+let topbtn = document.querySelector("#topbtn")
+
+topbtn.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+let openbtn=document.querySelector("#open");
+
+let closebtn=document.querySelector("#cross");
+
+let sidebar=document.querySelector(".sidebar")
+let overlay=document.querySelector("#overlay")
+
+openbtn.addEventListener("click",function(){
+        sidebar.classList.add("show")
+        overlay.classList.add("active")
+        document.body.classList.add("noscroll")
+})
+closebtn.addEventListener("click",function(){
+    sidebar.classList.remove("show")
+    overlay.classList.remove("active")
+    document.body.classList.remove("noscroll")
+})
+
+let openbtn2=document.querySelector("#opene")
+
+openbtn2.addEventListener("click",function(){
+    sidebar.classList.add("show")
+        overlay.classList.add("active")
+        document.body.classList.add("noscroll")
+})
+
+
+
+
+
